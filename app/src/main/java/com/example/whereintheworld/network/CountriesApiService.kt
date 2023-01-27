e03aa8c8f72f57b06792b69ceb6a664631663396
Volodymyr Balashov <xaosssss@gmail.com>
2023-01-27 10:44:39 +0900

    fixed data receiving

--- app/src/main/java/com/example/whereintheworld/network/CountriesApiService.kt
+++ app/src/main/java/com/example/whereintheworld/network/CountriesApiService.kt
@@ -6,5 +6,5 @@ import retrofit2.http.GET
 // Defining endpoints and required data type
 interface CountriesApiService {
     @GET("all")
-    fun getAllCountries(): List<Country>
+    suspend fun getAllCountries(): List<Country>
 }
